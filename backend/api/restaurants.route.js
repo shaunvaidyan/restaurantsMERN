37c1e4dcdc0cd1b746a20a2ed5f60c2385ebe754
Shaun Vaidyan <svaidyan24@gmail.com>
2022-03-13 13:07:50 -0500

    implemented mongoDB connection, express requests to return JSON restaurant data

---
+++ backend/api/restaurants.route.js
@@ -0,0 +1,15 @@
+import express from "express"
+import RestaurantsController from "./restaurants.controller.js"
+import ReviewsController from "./reviews.controller.js"
+
+const router = express.Router()
+
+router.route("/").get(RestaurantsController.apiGetRestaurants)
+
+router
+  .route("/review")
+  .post(ReviewsController.apiPostReview)
+  .put(ReviewsController.apiUpdateReview)
+  .delete(ReviewsController.apiDeleteReview)
+
+export default router
