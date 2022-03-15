aeddca1aa69bb0686ff191230e82d6f2893cdfe7
Shaun Vaidyan <svaidyan24@gmail.com>
2022-03-15 15:13:33 -0500

    bootstrapping app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import AddReview from "./components/AddReview";
+import Login from "./components/Login";
+import Restaurants from "./components/Restaurants";
+import RestaurantsList from "./components/RestaurantsList";
 
 function App() {
   return (
