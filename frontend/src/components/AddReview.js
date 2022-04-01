f518bd9ef8e2c890ae6104f716ac3110464d3d68
Shaun Vaidyan <svaidyan24@gmail.com>
2022-04-01 14:05:21 -0500

    fix import of useState and added initialReviewState

--- frontend/src/components/AddReview.js
+++ frontend/src/components/AddReview.js
@@ -1,9 +1,17 @@
-import React, {userState } from "react";
+import React, { useState } from "react";
 import RestaurantDataService from "../services/restaurant.js";
 import { Link } from "react-router-dom";
 
 const AddReview = props => {
-  
+  let initialReviewState = ""
+  let editing = false; // keeps track of whether this is new or editing review -- default is adding review
+
+  if (props.location.state && props.location.state.currentReview){
+    editing = true;
+    initialReviewState = props.location.state.currentReview.text
+  }
+
+  const [review, setReview] = useState(initialReviewState);
 }
 
 
