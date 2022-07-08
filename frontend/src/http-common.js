4bdafbf521b5457250702b790340a25dbc354f7d
Shaun Vaidyan <svaidyan24@gmail.com>
2022-07-08 16:29:29 -0500

    edited axios baseurl to avoid cors issues during deployment

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:5000/api/v1/restaurants",
+    baseURL: "https://nycgrub.vaidyan.me/api/v1/restaurants",
     headers: {
         "Content-type": "application/json"
     },
